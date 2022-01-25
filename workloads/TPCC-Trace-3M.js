93b8d672ab8ede1efce13a514d85b612c93b82be
Gustavo Moraes <gustavomoraes94@gmail.com>
2022-01-25 17:08:31 -0300

    Add files via upload

---
+++ workloads/TPCC-Trace-3M.js
@@ -0,0 +1,10 @@
+var test = [
+
+{"name_p":"MQ","buffer_size":1000,"hits":131279,"writes":90279,"time":9.265700},
+{"name_p":"MQ","buffer_size":2000,"hits":161444,"writes":89552,"time":15.435148},
+{"name_p":"MQ","buffer_size":4000,"hits":200985,"writes":88632,"time":27.530758},
+{"name_p":"MQ","buffer_size":8000,"hits":244075,"writes":87975,"time":50.829910},
+{"name_p":"MQ","buffer_size":16000,"hits":305885,"writes":87342,"time":100.553844},
+{"name_p":"MQ","buffer_size":32000,"hits":414091,"writes":86060,"time":191.075757},
+
+]
---
+++ workloads/TPCE-Trace-3M.js
@@ -0,0 +1,10 @@
+var test = [
+
+{"name_p":"MQ","buffer_size":1000,"hits":150783,"writes":85236,"time":9.005210},
+{"name_p":"MQ","buffer_size":2000,"hits":157883,"writes":85145,"time":14.721673},
+{"name_p":"MQ","buffer_size":4000,"hits":165250,"writes":84978,"time":26.874652},
+{"name_p":"MQ","buffer_size":8000,"hits":175823,"writes":84673,"time":51.910042},
+{"name_p":"MQ","buffer_size":16000,"hits":195591,"writes":84119,"time":98.123367},
+{"name_p":"MQ","buffer_size":32000,"hits":233011,"writes":83587,"time":191.286920},
+
+]
